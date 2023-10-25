26db2ebad3f6f792dac138bc598ef13f0f8d069f
keua Xavier <keuaxavierss@gmail.com>
2023-10-25 17:16:52 -0300

    standardized publication controllers

---
+++ src/controllers/publications/index.ts
@@ -0,0 +1,99 @@
+import { Request, Response } from "express";
+import prisma from "../../database";
+import { BadRequestError, NotFoundError } from "../../helpers/api-error";
+
+let page: number = 1;
+
+export default class PublicationController {
+
+    async createPublication(req: Request, res: Response) {
+        const { profileId, file, description, public_likes, public_comments } = req.body;
+
+        const userProfile = await prisma.profiles.findUnique({
+            where: { id: parseInt(profileId) },
+        });
+
+        if (!userProfile) throw new BadRequestError("perfil não encontrado");
+
+        const date_tamp = new Date();
+
+        const publication = await prisma.publications.create({
+            data: {
+                profileId,
+                name: userProfile!.name,
+                user_name: userProfile!.userName,
+                photo_profile: userProfile?.photo,
+                profile_checked: userProfile!.profileChecked,
+                date_tamp,
+                file,
+                description,
+                public_likes,
+                public_comments,
+            },
+        });
+
+        return res.json(publication);
+    }
+
+    async getPublication(req: Request, res: Response) {
+        const id = req.params.id;
+
+        const publication = await prisma.publications.findUnique({
+            where: { id: parseInt(id) },
+        });
+
+        if (!publication) throw new NotFoundError("Publicação não encontrada!");
+
+        return res.json(publication);
+    }
+
+    async listPublications(req: Request, res: Response) {
+        const publication = await prisma.publications.findMany({
+            skip: (page - 1) * 10,
+            take: 10,
+        });
+        if (!publication) throw new NotFoundError("Publicação não encontrada!");
+        if (publication.length === 0)
+            return res.json({ message: "Nenhuma publicação encontrada" });
+        page++;
+
+        return res.json(publication);
+    }
+
+    async editPublication(req: Request, res: Response) {
+        const id = req.params.id;
+        const { file, description, } = req.body;
+
+        const verifyPublication = await prisma.publications.findUnique({
+            where: { id: parseInt(id) }
+        })
+
+        if (!verifyPublication) throw new NotFoundError("Publicação não encontrada!")
+
+        const publication = await prisma.publications.update({
+            where: { id: parseInt(id) },
+            data: {
+                description,
+                file
+            }
+        })
+
+        return res.json(publication)
+    }
+
+    async deletePublication(req: Request, res: Response) {
+        const id = req.params.id;
+
+        const publication = await prisma.publications.findUnique({
+            where: { id: parseInt(id) },
+        });
+
+        if (!publication) throw new NotFoundError("Publicação não encontrada!");
+
+        await prisma.publications.delete({
+            where: { id: parseInt(id) },
+        });
+
+        return res.send();
+    }
+}
--- src/routers/publications/index.ts
+++ src/routers/publications/index.ts
@@ -1,28 +1,27 @@
 import { Router } from "express";
-import { CreatePublicationController } from "../../controllers/publicationsControllers/CreatePublicationController";
-import { GetPublicationController } from "../../controllers/publicationsControllers/GetPublicationController";
-import { EditPublicationController } from "../../controllers/publicationsControllers/EditPublicationCrontroller";
-import { DeletePublicationController } from "../../controllers/publicationsControllers/DeletePublicationController";
-import { ListPublicationController } from "../../controllers/publicationsControllers/ListPublicationController";
+import PublicationController from "../../controllers/publications";
 
 const publicationsRouter = Router();
 
 publicationsRouter.post(
   "/publications",
-  new CreatePublicationController().createPublication
+  new PublicationController().createPublication
 );
 publicationsRouter.get(
   "/publications/:id",
-  new GetPublicationController().getPublication
+  new PublicationController().getPublication
 );
 publicationsRouter.put(
   "/publications/:id",
-  new EditPublicationController().getPublication
+  new PublicationController().editPublication
 );
 publicationsRouter.delete(
   "/publications/:id",
-  new DeletePublicationController().getPublication
+  new PublicationController().deletePublication
 );
-publicationsRouter.get("/feed", new ListPublicationController().getPublication);
+publicationsRouter.get(
+  "/feed",
+  new PublicationController().listPublications
+);
 
 export default publicationsRouter;
