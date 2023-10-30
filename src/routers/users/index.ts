45dbabe6a50a967c58a397d4c6d2704aa55a3d95
josiemerson <josiemerson2013@gmail.com>
2023-10-30 11:27:46 -0300

    rota de account creada

--- src/routers/users/index.ts
+++ src/routers/users/index.ts
@@ -4,14 +4,16 @@ import { authMiddleware } from "../../middlewares/users/auth";
 
 const routerUser = Router();
 
-routerUser.post("/registerUser", new User().createUser);
+routerUser.post("/create", new User().createUser);
 
 routerUser.post("/login", new User().login);
+
+routerUser.put("/account/external",  new User().loginInFirebase);
 
 routerUser.get("/users", new User().listAllUser);
 
 routerUser.get("/users/:id", new User().listUserId);
 
-routerUser.put("/userModify/:id", authMiddleware, new User().updateUser);
+routerUser.put("/update/:id", authMiddleware, new User().updateUser);
 
 export default routerUser;
